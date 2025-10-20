68da4a3444eb73cdce61d4dfced123ce74e9a21d
yazdancode <yshabanei@gmail.com>
2025-10-20 09:21:35 +0330

    routing

--- system/router/routing.php
+++ system/router/routing.php
@@ -5,5 +5,11 @@ namespace System\Router;
 class Routing
 {
     private $current_route;
+
+    public function __construct()
+    {
+        global $current_route;
+        $this->current_route = explode('/', $current_route);
+    }
     
 }
