dade21f5b7f3704c1d508ebf2a27728de3b099b6
yazdancode <yshabanei@gmail.com>
2025-10-21 01:09:25 +0330

    i added gitignore and routing

--- system/router/routing.php
+++ system/router/routing.php
@@ -1,48 +1,60 @@
 <?php
 
 namespace System\Router;
+
+use ReflectionException;
 use ReflectionMethod;
 
 class Routing
 {
-    private $current_route;
+    private array $currentRoute;
 
-    public function __construct()
+    public function __construct(string $route)
     {
-        global $current_route;
-        $this->current_route = explode('/', $current_route);
+        $route = trim($route, '/');
+        $this->currentRoute = explode('/', $route);
     }
 
     public function run(): void
     {
-    $controller = basename($this->current_route[0]);
-    $path = realpath(dirname(__FILE__) . "/../../application/controllers/" . $controller . ".php");
-
-    if (!file_exists($path)) {
-        echo "404 - file not exists!";
-        exit;
-    }
-    require_once $path;
-    $method = sizeof($this->current_route) === 1 ? "index" : $this->current_route[1];
-    $class = "Application\\Controllers\\" . $controller;
-    $object = new $class();
-    if(method_exists($object, $method))
-    {
-        $reflection = new ReflectionMethod($class, $method);
-        $parameterCount = $reflection->getNumberOfParameters();
-        if($parameterCount <= count(array_slice($this->current_route, 2)))
-        {
-            call_user_func_array(array($object, $method), array_slice($this->current_route, 2));
+        $controller = ucfirst(basename($this->currentRoute[0] ?? 'Home'));
+        $controllerFile = dirname(__DIR__, 2) . "/application/controllers/$controller.php";
+        if (!file_exists($controllerFile)) {
+            http_response_code(404);
+            echo "404 - Controller file not found!";
+            return;
         }
-        else
-        {
-            echo "404 - parameter error!";
-        }
-    }
-    else
-    {
-        echo "404 - method not exist!";
-    }
 
+        require_once $controllerFile;
+
+        $className = "Application\\Controllers\\$controller";
+        if (!class_exists($className)) {
+            http_response_code(404);
+            echo "404 - Controller class not found!";
+            return;
+        }
+
+        $object = new $className();
+        $method = $this->currentRoute[1] ?? 'index';
+        if (!method_exists($object, $method)) {
+            http_response_code(404);
+            echo "404 - Method not found!";
+            return;
+        }
+
+        try {
+            $reflection = new ReflectionMethod($className, $method);
+        } catch (ReflectionException $e) {
+            http_response_code(500);
+            echo "500 - Reflection error!";
+            return;
+        }
+        $params = array_slice($this->currentRoute, 2);
+        if (count($params) < $reflection->getNumberOfRequiredParameters()) {
+            http_response_code(400);
+            echo "400 - Missing required parameters!";
+            return;
+        }
+        call_user_func_array([$object, $method], $params);
     }
 }
