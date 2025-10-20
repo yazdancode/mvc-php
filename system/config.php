a961d0385475e49a919a269af991e27ef5c38ccf
yazdancode <yshabanei@gmail.com>
2025-10-20 09:20:47 +0330

    routing

---
+++ system/config.php
@@ -0,0 +1,8 @@
+<?php
+
+$base_url = "http://localhost/mvc/";
+$base_dir = "/mvc/";
+
+$tmp = explode('?', $_SERVER['REQUEST_URI']);
+$current_route = str_replace($base_dir,'', $tmp[0]);
+unset($tmp);
---
+++ system/router/routing.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace System\Router;
+
+class Routing
+{
+    private $current_route;
+    
+}
