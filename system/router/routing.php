34a32b765c803cf18df47e22cb6aac635d263332
yazdancode <yshabanei@gmail.com>
2025-10-21 00:45:33 +0330

    routing

--- system/router/routing.php
+++ system/router/routing.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace System\Router;
+use ReflectionMethod;
 
 class Routing
 {
@@ -24,6 +25,24 @@ class Routing
     require_once $path;
     $method = sizeof($this->current_route) === 1 ? "index" : $this->current_route[1];
     $class = "Application\\Controllers\\" . $controller;
-    
+    $object = new $class();
+    if(method_exists($object, $method))
+    {
+        $reflection = new ReflectionMethod($class, $method);
+        $parameterCount = $reflection->getNumberOfParameters();
+        if($parameterCount <= count(array_slice($this->current_route, 2)))
+        {
+            call_user_func_array(array($object, $method), array_slice($this->current_route, 2));
+        }
+        else
+        {
+            echo "404 - parameter error!";
+        }
+    }
+    else
+    {
+        echo "404 - method not exist!";
+    }
+
     }
 }
