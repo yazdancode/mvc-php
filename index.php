042157f4f110594e9af1ac76cf750db119ba9714
yazdancode <yshabanei@gmail.com>
2025-10-24 12:42:00 +0330

    index

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
 include('system/config.php');
 include(__DIR__.'/system/router/routing.php');
-include('application/controllers/Home.php');
+include('application/controllers/home.php');
 include('system/bootstrap/boot.php');
